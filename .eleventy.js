1e59d09bbfdc97c4426a7851f94813dc46ad995f
Palina Skakun <palinaskakun@gmail.com>
2024-01-22 20:59:42 -0500

    Fixed h1 position but still using weird linking

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("screen.css");
-    eleventyConfig.addPassthroughCopy("images/*.png");
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("eleventy.js");
+    eleventyConfig.addPassthroughCopy("moon.html");
+    // eleventyConfig.addPassthroughCopy("index.html");
   };
