ea7cb16354dd389be651201eac7d41bbb088f5ca
Palina Skakun <palinaskakun@gmail.com>
2024-02-05 10:50:21 -0500

    commented out addpassthrough copy moon.html

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("screen.css");
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("eleventy.js");
-    eleventyConfig.addPassthroughCopy("moon.html");
+    // eleventyConfig.addPassthroughCopy("moon.html");
     // eleventyConfig.addPassthroughCopy("index.html");
   };
